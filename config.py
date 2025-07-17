83f814b0f1fa3a3e7f099d6712cc7699643a3287
8092821983rishu <rishuuuuuuuuuuuuuuuuuuuuu@gmail.com>
2025-07-17 11:35:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TEAM-ISTKHAR/IstkharMusic",
+    "https://github.com/8092821983rishu/IstkharMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
